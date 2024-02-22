3890198c9d4f0a7293491537dc94a048a7b2b90c
ashwire <arita.a.wawire@gmail.com>
2024-02-22 16:57:12 +0300

    initial commit

---
+++ hospitalapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def form(request):
+    return render(request,'form.html')
+
+def carousel(request):
+    return render(request,'carousel.html')
